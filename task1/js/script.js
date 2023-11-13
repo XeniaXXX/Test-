14f9e3db395f733de84e946622ff16f680cb4ae7
XeniaXXX <shiestakova97@list.ru>
2023-11-13 10:35:27 +0300

    init

--- task1/js/script.js
+++ task1/js/script.js
@@ -5,11 +5,7 @@ let header = document.querySelector('.header'),
     blueBlock = document.querySelector('#left'),
     yellowBlock = document.querySelector('#right'),
     modal = document.querySelector('.modal'),
-    modalClose = document.querySelector('.modal__close'),
-    leftCol = document.querySelector('.left'),
-    rightCol = document.querySelector('.right'),
-    newBlueBlock = document.querySelector('#left').cloneNode(),
-    newYellowBlock = document.querySelector('#right').cloneNode();
+    modalClose = document.querySelector('.modal__close');
 
 // №2.1
 yellowBtn.addEventListener('click', function() {
@@ -17,63 +13,14 @@ yellowBtn.addEventListener('click', function() {
 });
 
 // №2.2
-// 1
 greenBtn.addEventListener('click', function() {
-    if (blueBlock.classList.contains('blue') && yellowBlock.classList.contains('yellow')) {
-        blueBlock.classList.remove('blue');
-        blueBlock.classList.add('yellow');
-        yellowBlock.classList.remove('yellow');
-        yellowBlock.classList.add('blue');
+    if (getComputedStyle(blueBlock).order == 1) {
+        blueBlock.style.order = 2;
+        yellowBlock.style.order = 1;
     } else {
-        blueBlock.classList.remove('yellow');
-        blueBlock.classList.add('blue');
-        yellowBlock.classList.remove('blue');
-        yellowBlock.classList.add('yellow');
+        blueBlock.style.order = 1;
+        yellowBlock.style.order = 2;
     }
-});
-
-// 2
-greenBtn.addEventListener('click', function() {
-    if(leftCol.firstElementChild.classList == 'colored blue') {
-        leftCol.insertAdjacentElement('afterbegin', yellowBlock);
-        rightCol.insertAdjacentElement('afterbegin', blueBlock);
-    } else {
-        leftCol.insertAdjacentElement('afterbegin', blueBlock);
-        rightCol.insertAdjacentElement('afterbegin', yellowBlock);
-    }
-});
-
-// 3
-greenBtn.addEventListener('click', function() {
-    if(leftCol.firstElementChild.classList == 'colored blue') {
-        blueBlock.replaceWith(newYellowBlock);
-        yellowBlock.replaceWith(newBlueBlock);
-    } 
-    else {
-        newBlueBlock.replaceWith(yellowBlock);
-        newYellowBlock.replaceWith(blueBlock);
-    }
-});
-
-// 4
-$(function(){
-    jQuery.fn.swap = function(b) {
-      b = jQuery(b)[0];
-      let a = this[0],
-          a2 = a.cloneNode(true),
-          b2 = b.cloneNode(true),
-          stack = this;
-  
-      a.parentNode.replaceChild(b2, a);
-      b.parentNode.replaceChild(a2, b);
-  
-      stack[0] = a2;
-      return this.pushStack( stack );
-  };
-
-  $('.btn-success').on('click', function(){
-    $('#right').swap('#left');
-  });
 });
 
 // №2.3
--- task2/script.js
+++ task2/script.js
@@ -1,17 +1,17 @@
 // №2.5
 // №2.6
 let form = document.querySelector('form'),
-    inputs = document.querySelectorAll('input'),
-    formDataValues = document.querySelector('.form_data');
+    dataDiv = document.querySelector('.form_data');
 
 //XMLHttpRequest
 form.addEventListener('submit', function(event) {   
     event.preventDefault();
     let formData = new FormData(form),
-    json = JSON.stringify(Array.from(formData));
-    formDataValues.textContent = json;
+    values = Object.fromEntries(formData.entries()),
+    json = JSON.stringify(values);
+    dataDiv.textContent = json;
     let xhr = new XMLHttpRequest();
-    xhr.open("GET", "server.php", true);
+    xhr.open("GET", `server.php?${json}`, true);
     xhr.onload = function () {
         if (xhr.readyState === 4 && xhr.status === 200) {
             alert(xhr.responseText);
@@ -19,20 +19,39 @@ form.addEventListener('submit', function(event) {
         else {
             console.error(xhr.statusText);
         }
-    }
+    };
     xhr.send();
 });
 
+
+// ajax
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     let formData = new FormData(form),
+//     values = Object.fromEntries(formData.entries()),
+//     json = JSON.stringify(values);
+//     dataDiv.textContent = json;
+//     $.ajax({
+//         url: 'server.php', 
+//         method: 'GET', 
+//         data: json,
+//         success: function(data) {
+//             alert(data);
+//         }
+//     });
+// });
+
 //fecth 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    let formData = new FormData(form),
-    json = JSON.stringify(Array.from(formData));
-    formDataValues.textContent = json;
-    fetch("server.php")
-    .then(data => data.text())
-    .then(data => {
-        alert(data);
-    });
-});
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     let formData = new FormData(form),
+//     values = Object.fromEntries(formData.entries()),
+//     json = JSON.stringify(values);
+//     dataDiv.textContent = json;
+//     fetch(`server.php?${json}`)
+//     .then(data => data.text())
+//     .then(data => {
+//         alert(data);
+//     });
+// });
 
